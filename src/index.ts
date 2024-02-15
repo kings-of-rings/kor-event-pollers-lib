158b803de16deb4555b277803309d7b1a5732938
XDapps <xdev@xdapps.xyz>
2024-02-15 16:13:01 -0500

    8

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export * from './models/poll-managers/ERC20PollManager';
 export * from './models/erc-20-pollers/ERC20TransferPoller';
+export * from './models/erc-1155-pollers/ERC1155TransferSinglePoller';
+export * from './models/draft-pollers/DraftControllerPoller';
