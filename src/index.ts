6d14fa3a1cd8b1b2ec3f0b9b88aef84ad063b9f6
XDapps <xdev@xdapps.xyz>
2024-02-16 00:13:57 -0500

    10

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ export * from './models/poll-managers/ERC20PollManager';
 export * from './models/erc-20-pollers/ERC20TransferPoller';
 export * from './models/erc-1155-pollers/ERC1155TransferSinglePoller';
 export * from './models/draft-pollers/DraftControllerPoller';
+export * from './models/registry-pollers/AthleteRegistryPoller';
+export * from './models/registry-pollers/CollegeRegistryPoller';
+export * from './models/registry-pollers/ProRegistryPoller';
+export * from './models/registry-pollers/KoRDirectoryPoller';
